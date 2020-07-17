16473f81a8f8af39a4ab01f4dd6fe5a26cf41c85
Dimalis24 <67799855+Dimalis24@users.noreply.github.com>
2020-07-17 20:53:04 +0300

    Add files via upload

---
+++ const Langs = {.js
@@ -0,0 +1,55 @@
+const Langs = {
+    'ru': 'Russian',
+    'en': 'English',
+    'ua': 'Ukrain',
+    'de': 'German',
+};
+
+const Country = {
+    'Minsk': 'Berlin',
+    'Berlin': 'German',
+    'Kiev': 'Ukrain',
+};
+
+for (const city in Country) {
+    console.log('${city} - this ${Country[city]}')
+}
+
+const arrNums = [5, 8, 24, 16, 37, 45, 3, 4, 34, 98];
+
+function Nums (arrNums, ) {
+    if () {
+        return Math.max(...arrNums);
+    } 
+return Math.min(...arrNums);
+}
+
+const array = [];
+console.log(array)
+
+for (let i = 0; i < 10; i++) {
+    (i);
+  }
+  
+
+
+
+let ourDo = 0;
+const Do = [12, 15, 20, 25, 59, 79];
+Do.forEach(num =>  ourDo += num);
+
+console.log(
+    ourDo / Do.length
+);
+
+ 
+const palindrome = str => {
+    
+    str = str.toLowerCase(anna)
+
+    return str === str.split('').reverse().join('')
+  }
+    
+
+function isPalindrome(anna)
+console.log(`anna is ${apalindrome('anna')});
